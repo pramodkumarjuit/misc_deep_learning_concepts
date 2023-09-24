ca0e7e30f73898fdd840e9df39db97e168650b4f
Pramod Kumar <80332478+pramodkumarjuit@users.noreply.github.com>
2023-09-24 14:57:22 +0530

    print loss for each input in the batch

--- cross_entropy_example.py
+++ cross_entropy_example.py
@@ -16,10 +16,33 @@ predicted_scores = torch.tensor([
     [0.3, 0.2, 0.4, 0.1]
 ], dtype=torch.float32)
 
-# Define the CrossEntropyLoss
+# CrossEntropyLoss function calculates a single loss value
+# that represents the average loss across all the inputs in the batch.
+# If you want to compute the total loss for a specific batch,
+# you would simply multiply this average loss by the batch size (3 here).
+
 criterion = nn.CrossEntropyLoss()
 
 # Calculate the loss
 loss = criterion(predicted_scores, true_labels)
-
 print("Cross-Entropy Loss:", loss.item())
+
+# How to print loss for each of the input in a batch?
+# Calculate individual losses for each input
+individual_losses = []
+
+# Iterate over the batch
+for i in range(len(true_labels)):
+    # Get the true label and predicted scores for the current input
+    true_label = true_labels[i]
+    predicted_score = predicted_scores[i]
+
+    # Calculate the loss for the current input
+    loss = criterion(predicted_score.unsqueeze(0), torch.tensor([true_label]))
+
+    # Append the loss to the list
+    individual_losses.append(loss.item())
+
+# Print individual losses
+for i, loss in enumerate(individual_losses):
+    print(f"Loss for input {i}: {loss}")
